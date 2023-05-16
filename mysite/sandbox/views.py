f9f880cc4d3ddcf0a4a3cbcfdf579505328496e2
Redas Miliunas <redas.miliunas@outlook.com>
2023-05-16 14:59:00 +0300

    Produktu sar atvaizdavimas padarytas (base.html failo ikelimas)

--- mysite/sandbox/views.py
+++ mysite/sandbox/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+def products(request):
+    context = {
+        'products': Product.objects.all()
+    }
+    return render(request, 'products.html', context=context)
