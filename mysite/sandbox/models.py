fabe0c50c40909cafe3d6876fc6199db40164c52
Redas Miliunas <redas.miliunas@outlook.com>
2023-06-05 15:17:25 +0300

    Vartotojo foto automatiskai sumazinama

--- mysite/sandbox/models.py
+++ mysite/sandbox/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from datetime import date
 from tinymce.models import HTMLField
+from PIL import Image
 
 # Create your models here.
 
@@ -132,6 +133,13 @@ class UserProfile(models.Model):
     user = models.OneToOneField(to=User, verbose_name='User', on_delete=models.CASCADE)
     picture = models.ImageField(verbose_name='Picture', upload_to='profile_pics', default='profile_pics/default.png')
 
+    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+        super().save(force_insert, force_update, using, update_fields)
+        img = Image.open(self.picture.path)
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.picture.path)
 
     def __str__(self):
         return f'{self.username} profile'
